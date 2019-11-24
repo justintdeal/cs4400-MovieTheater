1e2cd65228a2a91d428606303dd73636e8aac149
Justin Deal <jdeal1912@gmail.com>
2019-11-24 16:49:23 -0500

    dynamic credit card add and added to api

--- movie.py
+++ movie.py
@@ -1,2 +1,3 @@
 from theater import app
+app.secret_key="afd34cf"
 app.run(debug=True)
--- theater/src/api.py
+++ theater/src/api.py
@@ -1,12 +1,8 @@
 from theater import app
 import theater.src.dbConnection as db
-from flask import render_template, request, flash, url_for
+from flask import render_template, request, flash, url_for, redirect
 
 #screen 1: login
-#need to validate login here
-#if login valid, redirect to correct dashboard
-#else flash error message and return 
-
 @app.route('/', methods=['GET', 'POST'])
 def index():
     if request.method == 'GET':
@@ -15,40 +11,83 @@ def index():
         user = request.form['email']
         password = request.form['password']
         #user (username, status, isCustomer, isAdmin, isManager)
-        # user = db.userLogin(user, password)
-        # if user[0] == None:
-        #     flash('Invalid Login')
-        #     return render_template('home.html')
-        # if user[0][2]:
-        return render_template('home.html')
+        user = db.userLogin(user, password)
+        if user[0] == None:
+            flash('Invalid Login')
+            return render_template('home.html')
+        user=user[0]
+        return redirect(url_for('dashboard', user = user))
 
 #screen 2
-#finished
 @app.route("/register")
 def regiserHome():
     return render_template('registerHome.html')
 
 #screens 3-6
-# make sure username is unique
-# make sure password is 8 characters
-# hash passwords
+
 # unique credit card numbers
 # # credit card >=1 && <=5 and lenght == 16
-# unique address
 # populate company dropdown
 @app.route("/register/<role>", methods=['GET', 'POST'])
 def registerRole(role):
     if request.method == 'GET':
         return getRegTemplate(role)
+    else:
+        if role == "user":
+            password = request.form['password']
+            confPass = request.form['confPass']
+            first = request.form['first']
+            last = request.form['last']
+            username = request.form['username']
+    
+            if password == confPass and len(password) >= 8:
+                db.userRegister(username, password, first, last)
+            elif len(password) <= 8:
+                print(10)
+                message = "Password must be at least 8 characters"
+                return render_template("userReg.html", messages=message)
+            else:
+                message = "Passwords must match"
+                return getRegTemplate("user", message)
+        if role == "customer":
+            password = request.form['password']
+            confPass = request.form['confPass']
+            first = request.form['first']
+            last = request.form['last']
+            username = request.form['username']
+            return "hello"
+
+
+def getRegTemplate(role, messages=None):
+    if role == 'user':
+        return render_template('userReg.html', messages=messages)
+    elif role == "manager":
+        return render_template('manReg.html', messages=messages)
+    elif role == "customer":
+        return render_template('custReg.html', messages=messages)
+    elif role == "mancust":
+        return render_template('custManReg.html', messages=message)
 
 #screens 7-12
 @app.route("/dashboard/")
-def dashboard():
-    types = ['Manager', 'Cust', 'Admin', 'User', 'AdminCust', 'ManagerCust']
-
-    userType = types[4]
+def dashboard(user):
+    #user (username, status, isCustomer, isAdmin, isManager)
+    userType = getUserType(user)
     template = "dash"+ userType +".html"
     return render_template(template)
+
+def getUserType(user):
+    if user[3] and not user[2] and not user[4]:
+        return "Admin"
+    if user[3] and user[2] and not user[4]:
+        return "AdminCust"
+    if user[4] and not user[2] and not user[3]:
+        return "Manager"
+    if user[2] and user[4] and not user[3]:
+        return "ManagerCust"
+    if user[2] and not user[4] and not user[3]:
+        return "Customer"
+    return "User"
 
 #screen 13
 # can approve pending/declined users
@@ -109,26 +148,3 @@ def viewHistory():
 @app.route("/visit/history", methods=['GET', 'POST'])
 def visitHistory():
     return render_template('visitHistory.html')
-
-
-
-#helpers
-def getRegTemplate(role):
-    if role == 'user':
-        return render_template('userReg.html')
-    elif role == "manager":
-        return render_template('manReg.html')
-    elif role == "customer":
-        return render_template('custReg.html')
-    elif role == "mancust":
-        return render_template('custManReg.html')
-
-
-def validateEmail(email):
-    regx = '^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$'
-    if (re.search(regx, email)):
-        return True
-    return False
-
-def validatePass(password, confPass):
-    return 1
